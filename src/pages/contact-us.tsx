45e43213c03b2b8a742af2756e0e59c71d909b94
DelaBandara <120809445+DelaBandara@users.noreply.github.com>
2023-10-10 12:43:32 +0530

    Update contact-us.tsx

--- src/pages/contact-us.tsx
+++ src/pages/contact-us.tsx
@@ -5,6 +5,7 @@ import Link from "next/link";
 import { MdPhoneAndroid } from "react-icons/md";
 
 
+
 const Contact = () => {
     return (
         <>
@@ -13,6 +14,34 @@ const Contact = () => {
             <div className={`d-flex w-100  align-items-center flex-column p-5 ${styles.sec_abut}`}>
             <h2 className='my-4'>Contact Us</h2>
             </div>
+          </div>
+          <div className="row">
+          <div className="col-12 col-lg-6" >
+          <div className="contact-info">
+                <h2>Contact Information</h2>
+                <p>Bring the table winwin survival strategies ensure<br/> 
+                   proactive domination the end of the day<br/>  
+                   going real times multiple touchpoints.</p>
+                <div className="contact-icons">
+                  <div className="contact-icon">
+                  <i className="fa fa-map-marker" aria-hidden="true"></i>
+                    <p>Riverside 25, San Francisco, California</p>
+                  </div>
+                  <div className="contact-icon">
+                  <i className="fa fa-envelope" aria-hidden="true"></i>
+                    <p>evamattew@mail.com</p>
+                  </div>
+                  <div className="contact-icon">
+                  <i className="fa fa-phone" aria-hidden="true"></i>
+                    <p>800-234-567</p>
+                  </div>
+                </div>
+          </div>
+          </div>
+          <div className="col-12 col-lg-6">
+hi
+          </div>
+          
           </div>  
 
 
